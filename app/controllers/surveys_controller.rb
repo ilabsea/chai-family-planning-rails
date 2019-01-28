87b4cb769698b1c1aa6773446012d6e680755105
mouyleng <mouyleng@instedd.org>
2019-01-28 13:43:16 +0700

    fix csv download get duplication questions

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,7 +6,7 @@ class SurveysController < ApplicationController
       @fully_form_version = params[:version].present? ? params[:version] : @versions.first.form_version
       @surveys = Survey.where(version: @fully_form_version).order(start_entried_at: :desc).page(params[:page])
       @version_id = Version.from_fully_form_version(@fully_form_version).id
-      @headers = Question.where(version_id: @version_id).order(:order).pluck(:name)
+      @headers = Survey.headers(@version_id)
     end
 
     respond_to do |format|
